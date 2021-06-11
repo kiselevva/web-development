410058eddaa5fd80fb55bb2f647ed4dda0a81abe
kiselevva <80060751+kiselevva@users.noreply.github.com>
2021-06-11 11:45:22 +0300

    Add files via upload
    исправлен калькулятор

--- lw8/calc.js
+++ lw8/calc.js
@@ -1,7 +1,7 @@
-const act = ['+', '-', '*', '/']
+const arithmeticOperation = ['+', '-', '*', '/']
 
-function math(act, num1, num2) {
-    switch(act) {
+function math(arithmeticOperation, num1, num2) {
+    switch(arithmeticOperation) {
         case '+':
             result = num1 + num2;
             break;
@@ -19,35 +19,19 @@ function math(act, num1, num2) {
     return result = String(result);
 }
 
-function arithmeticAction(stack, i) {
+function operationIdentify(stack, i) {
     let num1 = Number(stack[i + 1]);
     let num2 = Number(stack[i + 2]);
     math(stack[i], num1, num2);
-    stack.splice(i, 1);
-    stack.splice(i, 1);
-    stack.splice(i, 1);
-    stack.splice(i, 0, result); 
-    if (stack[i - 1] == '(' ) {
-        stack.splice(i - 1, 1);
-        if (stack[i] == ')' ) {
-            stack.splice(i, 1);
-        }
-    }
+    stack.splice(i, 3);
+    stack.splice(i, 0, result);
     return(stack);
 }
 
-function calculation(stack, i, length) {
+function calculate(stack, i, length) {
     while (i <= length) {
-        if (stack[i + 1] == '(') {
-            i = i + 1;
-            continue;     
-        }
-        if (stack[i + 2] == '(') {
-            i = i + 2;
-            continue;
-        }
-        if (act.includes(stack[i])) {
-            arithmeticAction(stack, i)
+        if (arithmeticOperation.includes(stack[i])) {
+            operationIdentify(stack, i)
             i = 0;
             continue;    
         }
@@ -57,22 +41,20 @@ function calculation(stack, i, length) {
 }
 
 function calc(string) {
+    const length = stack.length;
     let stack = string.split('');
-    let length = stack.length;
-    let elementOfArr = 0;
+    let indexOfArr = 0;
     let val = ' ';
-    while (elementOfArr <= length) {
-        if (stack[elementOfArr] == val) {
-            stack.splice(elementOfArr, 1);
+    while (indexOfArr <= length) {
+        if (stack[indexOfArr] == val) {
+            stack.splice(indexOfArr, 1);
         } else {
-            elementOfArr = elementOfArr + 1; 
+            indexOfArr = indexOfArr + 1;
         }
     }
     let i = 0;
     if (length > 2) {
-        calculation(stack, i, length);
+        calculate(stack, i, length);
         return(stack[0]);
-    } else {
-        console.log('error')
     }
 }
